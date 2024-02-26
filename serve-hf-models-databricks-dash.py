95a127dc2614f9c9d0ee68dba87848811fc6606a
Sachin Seth <139485142+sachds@users.noreply.github.com>
2024-02-26 15:04:46 -0500

    Update serve-hf-models-databricks-dash.py

--- serve-hf-models-databricks-dash.py
+++ serve-hf-models-databricks-dash.py
@@ -41,7 +41,7 @@ from mlflow.tracking import MlflowClient
 
 
 # Ensure dbutils is available and you have a widget named 'model' set up
-model_name = dbutils.widgets.get("model")
+model_name = dbutils.widgets.get("model_name")
 
 # Initialize the pipeline with the model specified in the widget, assuming it's a valid model name
 text_generation_pipeline = pipeline(task='text-generation', model=model_name, pad_token_id=50256, device=0)  # Use device=0 for GPU or device=-1 for CPU
